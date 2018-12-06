80cb5c23d71f3e1815dd5fe278a9da21943b1c23
hariprasath <hariprasath.sivanandam@gmail.com>
2018-12-06 01:14:41 -0500

    added all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseGridComponent } from './course-grid/course-grid.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import { TopicPillsComponent } from './topic-pills/topic-pills.component';
+import { WidgetListComponent } from './widget-list/widget-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseGridComponent,
+    ModuleListComponent,
+    LessonTabsComponent,
+    TopicPillsComponent,
+    WidgetListComponent
   ],
   imports: [
     BrowserModule
